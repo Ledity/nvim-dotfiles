de1eb98d1bbab9ed25709d8a5dc761057ffb301d
Andrew Voeyko <andrew.voeicko@yandex.ru>
2026-07-28 15:47:42 +0300

    Descriptions to several keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,14 +4,13 @@ vim.g.mapleader = ' '
 
 -- WINDOW
 local window_keymaps = {
-    { 'n', '<leader>w-', function() vim.cmd('resize -2') end },
-    { 'n', '<leader>w+', function() vim.cmd('resize +2') end },
-    { 'n', '<leader>w<', function() vim.cmd('vertical resize -5') end },
-    { 'n', '<leader>w>', function() vim.cmd('vertical resize +5') end },
-    { 'n', '<leader>wt', '<c-w>wT' },
-    { 'n', '<leader>wv', vim.cmd.vnew },
-    { 'n', '<leader>ws', vim.cmd.new },
-}
+    { 'n', '<leader>w-', function() vim.cmd('resize -2') end, { desc = 'Decrease this window\'s height' } },
+    { 'n', '<leader>w+', function() vim.cmd('resize +2') end, { desc = 'Increase this window\'s height' } },
+    { 'n', '<leader>w<', function() vim.cmd('vertical resize -5') end, { desc = 'Decrease this window\'s width' } },
+    { 'n', '<leader>w>', function() vim.cmd('vertical resize +5') end, { desc = 'Increase this window\'s width' } },
+    { 'n', '<leader>wv', vim.cmd.vnew, { desc = 'Split the window vertically' } },
+    { 'n', '<leader>ws', vim.cmd.new, { desc = 'Split the window horizontally' } },
+}kkk
 
 for _, action in ipairs { 'c', 'h', 'j', 'k', 'l', 'H', 'J', 'K', 'L', 'o', 'p', 'q', 'w', '=' } do
     table.insert(window_keymaps, { 'n', '<leader>w' .. action, '<C-w>'..action })
@@ -19,20 +18,20 @@ end
 
 -- TABS
 local tab_keymaps = {
-    { 'n', '<leader>tn', vim.cmd.tabnew },
-    { 'n', '<leader>tq', vim.cmd.tabclose },
-    { 'n', '<leader>th', vim.cmd.tabfirst },
-    { 'n', '<leader>tj', vim.cmd.tabnext },
-    { 'n', '<leader>tk', vim.cmd.tabprevious },
-    { 'n', '<leader>tl', vim.cmd.tablast },
+    { 'n', '<leader>tn', vim.cmd.tabnew, { desc = 'Open new tab' } },
+    { 'n', '<leader>tq', vim.cmd.tabclose, { desc = 'Close this tab' } },
+    { 'n', '<leader>th', vim.cmd.tabfirst, { desc = 'Switch to the first tab' } },
+    { 'n', '<leader>tj', vim.cmd.tabnext, { desc = 'Switch to the next tab' } },
+    { 'n', '<leader>tk', vim.cmd.tabprevious, { desc = 'Switch to the previous tab' } },
+    { 'n', '<leader>tl', vim.cmd.tablast, { desc = 'Switch to the last tab' } },
 }
 
 -- BUFFERS
 local buffer_keymaps = {
-    { 'n', '<leader>h', vim.cmd.bfirst },
-    { 'n', '<leader>j', vim.cmd.bnext },
-    { 'n', '<leader>k', vim.cmd.bprevious },
-    { 'n', '<leader>l', vim.cmd.blast },
+    { 'n', '<leader>h', vim.cmd.bfirst, { desc = 'Switch to the first buffer' } },
+    { 'n', '<leader>j', vim.cmd.bnext, { desc = 'Switch to the next buffer' } },
+    { 'n', '<leader>k', vim.cmd.bprevious, { desc = 'Switch to the previous buffer' } },
+    { 'n', '<leader>l', vim.cmd.blast, { desc = 'Switch to the last buffer' } },
 }
 
 local keymaps = merge_arrays(window_keymaps, tab_keymaps, buffer_keymaps)
