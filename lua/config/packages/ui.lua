b183e7160f4da454d35ce686725dd7cb1c31ead2
Andrew Voeyko <andrew.voeicko@yandex.ru>
2026-07-30 17:31:56 +0300

    Add dashboard

--- lua/config/packages/ui.lua
+++ lua/config/packages/ui.lua
@@ -14,13 +14,30 @@ return {
     {
         src = 'https://github.com/akinsho/bufferline.nvim',
         config = function()
-            require('bufferline').setup {}
+            require('bufferline').setup {
+                options = {
+                    custom_filter = function(buf_number, buf_numbers)
+                        if vim.fn.bufname(buf_number) == 'homecoming://Dashboard' then
+                            return false
+                        end
+
+                        return true
+                    end,
+                },
+            }
         end
     },
     {
         src = 'https://github.com/vzze/cmdline.nvim',
         config = function()
             require('cmdline').setup {}
+        end
+    },
+    {
+        src = 'https://github.com/leo-alvarenga/homecoming.nvim',
+        -- config = 'Homecoming',
+        config = function()
+            require('homecoming-nvim').setup {}
         end
     }
 }
