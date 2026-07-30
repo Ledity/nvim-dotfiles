41ced3ddd454627cdd7c1bbac37fe6e5bbe805c8
Andrew Voeyko <andrew.voeicko@yandex.ru>
2026-07-30 16:26:54 +0300

    Treesitter support

--- lua/config/packages/code/treesitter.lua
+++ lua/config/packages/code/treesitter.lua
@@ -16,6 +16,15 @@ return {
         end
     },
     {
-        'https://github.com/windwp/nvim-ts-autotag'
-    }
+        src = 'https://github.com/windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup {
+                opts = {
+                    enable_close = true,
+                    enable_rename = true,
+                    enable_close_on_slash = true
+                }
+            }
+        end
+    },
 }
