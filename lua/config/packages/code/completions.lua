a34d194854e3883cf48dbeed55d4397fa567165b
Andrey Voeyko <andrew.voeicko@yandex.ru>
2026-08-31 10:48:59 +0300

    Add completion engine for Termux

--- lua/config/packages/code/completions.lua
+++ lua/config/packages/code/completions.lua
@@ -1,17 +1,32 @@
-return {
-    'https://github.com/saghen/blink.lib',
-    {
-        src = 'https://github.com/saghen/blink.cmp',
-        config = function()
-            local cmp = require 'blink.cmp'
-            cmp.build():pwait()
-            cmp.setup {
-                keymap = {
-                    preset = 'enter',
-                    ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
-                    ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+local is_termux = require('lib.commons').is_termux
+
+-- blink fails to compile on Termux
+if not is_termux() then
+    return {
+        'https://github.com/saghen/blink.lib',
+        {
+            src = 'https://github.com/saghen/blink.cmp',
+            config = function()
+                local cmp = require 'blink.cmp'
+                cmp.build():pwait()
+                cmp.setup {
+                    keymap = {
+                        preset = 'enter',
+                        ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
+                        ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+                    }
                 }
-            }
-        end
-    },
-}
+            end
+        },
+    }
+else
+    return {
+        {
+            src = 'https://github.com/brianaung/compl.nvim',
+            config = function()
+                vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+                require('compl').setup {}
+            end
+        }
+    }
+end
